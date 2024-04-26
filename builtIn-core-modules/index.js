81d00f7d4d1b77f537c999acfc8ce74943f1ac0f
Kunj-P-Sharma <conkush@nibe.se>
2024-04-26 21:46:52 +0530

    added the built in modules

---
+++ builtIn-core-modules/index.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+/**
+ * Built in modules are the modules that the nodes ships with
+ * They are also reffered to as core modules
+ * you have to import them before you can use them 
+ * const path = require("node:path"); prefix them with to signify that its a built in module
+ * The main core modules are
+    1. path module
+    2. events module
+    3. fs (file system) module
+    4. http
+ */
---
+++ builtIn-core-modules/pathMoudle.js
@@ -0,0 +1,6 @@
+const path = require("node:path");
+
+/**
+ * console.log(__dirname) => C:\personal\NodeJsBasics\builtIn-core-modules
+ * console.log(__filename) => C:\personal\NodeJsBasics\builtIn-core-modules\pathMoudle.js
+ */
